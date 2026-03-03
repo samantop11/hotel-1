71a6bb428f7d861ef7f508c371798b78d5d6321c
samantop11 <samantha.santos.alves@escola.pr.gov.br>
2026-03-03 08:39:50 -0300

    Add files via upload

--- ProjetoHotelSamantha 2b/backend/app.py
+++ ProjetoHotelSamantha 2b/backend/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+# caminho da pasta p dois pontos mandam isso a 1 pasta atras onde esta o front.
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 
+#pasta frontend
+FRONTEND_DIR = os.path.join(BASE_DIR,"frontend")
+
+# pasta static (css)
+STATIC_DIR= os.path.join(BASE_DIR, "static")
+
+app = Flask(__name__, static_folder=STATIC_DIR,static_url_path="/" + STATIC_DIR)
 
 @app.route("/")
 def home ():
